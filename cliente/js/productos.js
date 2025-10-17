361845207ecb430df825f7314f058c4064cf0c88
Sergio <sergiosanchez.24@campuscamara.es>
2025-10-17 11:56:44 +0200

    commit4

--- cliente/js/productos.js
+++ cliente/js/productos.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         <div class="card-body text-center">
           <h5 class="card-title">${p.nombre}</h5>
           <p><img id="img" style="width: 34%;" src="https://clientes.oxfamintermon.org/643-large_default/camiseta-hombre-lisa-algorg-blanca-s.jpg"/></p>
-          <h6 class="card-subtitle>${p.categoria}</p>
+          <h6 class="card-subtitle>${p.categoria}</h6>
           <p class="card-text">${p.descripcion}</p>
           <p class="fw-bold">${p.precio.toFixed(2)} €</p>
           <a href="producto.html?id=${p.id}" class="btn btn-primary">Ver detalle</a>
