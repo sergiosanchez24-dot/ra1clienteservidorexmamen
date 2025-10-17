1cb5f872227b9e19c7a380a6960e13647e20a3e9
Sergio <sergiosanchez.24@campuscamara.es>
2025-10-17 10:00:49 +0200

    commit2

--- servidor/api.php
+++ servidor/api.php
@@ -7,7 +7,7 @@ $productos = [
         "id" => 1,
         "nombre" => "Camiseta básica",
         "descripcion" => "Camiseta de algodón 100% en varios colores.",
-        "precio" => 12.99
+        "precio" => 12.99,
         "img" => "https://clientes.oxfamintermon.org/643-large_default/camiseta-hombre-lisa-algorg-blanca-s.jpg",
         "categoria" => "Camisetas"
     ],
@@ -15,7 +15,7 @@ $productos = [
         "id" => 2,
         "nombre" => "Pantalón vaquero",
         "descripcion" => "Pantalón de mezclilla azul clásico.",
-        "precio" => 29.95
+        "precio" => 29.95,
         "img" => "https://gusgusboutique.es/wp-content/uploads/2023/10/Vaqueros-azul-bajo-deshilachado-Salsa-Jeans-online.jpg",
         "categoria" => "Pantalones"
     ],
@@ -23,7 +23,7 @@ $productos = [
         "id" => 3,
         "nombre" => "Zapatillas deportivas",
         "descripcion" => "Zapatillas ligeras y cómodas para el día a día.",
-        "precio" => 45.50
+        "precio" => 45.50,
         "img" => "https://www.calzadoshermi.com/50433-large_default/zapatillas-ligeras-confort-hombre-9870c.jpg",
         "categoria" => "Zapatos"
     ],
@@ -39,7 +39,15 @@ if (isset($_GET['id'])) {
         }
     }
     echo json_encode(["error" => "Producto no encontrado"]);
- }else if (isset($_GET['modo']) && $_GET['modo'] === 'texto') {
+}else if(isset($_GET['max'])) {
+    header('Content-Type: application/json');
+    $max = 15;
+    $productosFiltrados = array_filter($productos, fn($p) => $p['precio'] <= $max);
+    echo json_encode($productosFiltrados);
+
+
+
+}else if (isset($_GET['modo']) && $_GET['modo'] === 'texto') {
     header('Content-Type: text/html; charset=UTF-8');
     mostrarProductosJSON($productos);
 } //Pista debes detectar el max con  el get para el ejercicio--> Ejercicio 4: Filtrado de productos por GET
@@ -52,7 +60,16 @@ else {
 //http://localhost/ra1clienteservidorexmamen/servidor/api.php?modo=texto
 function mostrarProductosJSON($productos) {
     echo "--- Lista de productos ---<br>";
+    echo "<br></br>";
     $json = json_encode($productos);
     $array = json_decode($json, true);
     //Crear un foreach para recorrerlo  y pintar por pantalla, el id, nombre y precio del producto
+
+    foreach($array as $elemento){
+        echo $elemento["nombre"];
+        echo " - ";
+        echo $elemento["precio"];
+        echo " €";
+        echo "<br></br>";
+    }
 }
